af36a2d048386833a6e09be6b58f153b19fd37de
Jinocvarughese <jinocv1999@gmail.com>
2021-09-10 12:47:32 +0530

    added order online scrollable menu

--- frontend/src/Page/Restaurant/OrderOnline.jsx
+++ frontend/src/Page/Restaurant/OrderOnline.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 
 //component
 import FloatMenubtn from '../../Components/restaurant/Order-Online/FloatMenubtn';
+import MenuListContainer from '../../Components/restaurant/Order-Online/MenuListContainer';
 
 const OrderOnline = () => {
     return (
         <>
           <div className="w-full">
-             <FloatMenubtn />
+            <aside className="hidden md:flex flex-col gap-3 overflow-y-scroll border-r border-gray-200 h-screen  w-1/4">
+              <MenuListContainer />
+              <MenuListContainer />
+              
+            </aside>
+            <div className="w-full md:w-3/4"></div>
           </div> 
+          <FloatMenubtn />
         </>
     )
 }
